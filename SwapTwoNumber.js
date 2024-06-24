93089574f65f456805c24e2846594ab74f7d8136
shravan kumar <slmalviya32@gmail.com>
2024-06-24 14:45:49 +0530

    commit

---
+++ SwapTwoNumber.js
@@ -0,0 +1,12 @@
+let a = 20;
+let b = 10;
+let temp;
+console.log(`before swapping a = ${a}`);
+console.log(`before swaaping b = ${b}`);
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(`after swapping a = ${a}`);
+console.log(`after swaaping b = ${b}`);
